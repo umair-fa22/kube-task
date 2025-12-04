1ba8394899e569ae7034aa1437b94dff07e0c93f
UMAIR ALI <umair.dev@hotmail.com>
2025-12-04 14:20:30 +0500

    Screenshots

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Kubernetes!\n');
+  res.end('Hello from Kubernetes! v2\n');
 });
 server.listen(8080, () => {
   console.log('Server running on port 8080');
